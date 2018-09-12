caecd9ab9f635e8191f0b77a8fbe29e48a2cedca
kevin leon <kevinoleonr@hotmail.com>
2018-09-12 18:29:13 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+var colors= ['red','blue','green'];
+var colors2=new Array('red','blue','green');
+
+colors[3]= 'green';
+colors.push('purple');
+
+var numbers= [1,2,3,4,5,6,7,8,9];
+
+for (var i = numbers.length - 1; i >= 0; i--) {
+	
+	//console.log(i);
+}
+var x=0;
+while(x<10){
+	//console.log(x);
+	x++;
+}
+
+
+//alert(colors);
+//alert(numbers.reverse());
